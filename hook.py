a59bd3bf75d63abaaa019e3008152b50e1f88ca6
Colleen Murphy <colleen.murphy@suse.com>
2020-02-13 17:05:38 -0800

    Add keystone functionality
    The python webhook now corresponds an incoming request namespace with a
    keystone project and queries the resource limit associated with it (pods
    only), as well as the current resource count in kubernetes, and denies
    the request if the requested claim exceeds the limit in keystone.

--- hook.py
+++ hook.py
@@ -2,11 +2,31 @@ import json
 import ssl
 import http.server
 
+import openstack
+import kubernetes.client
+
 
 class WebhookHandler(http.server.BaseHTTPRequestHandler):
+
+    openstack_conn = None
+
+    def _setup_openstack_connection(self):
+        if not self.openstack_conn:
+            self.openstack_conn = openstack.connect(cloud='devstack')
+
+    def _get_kubernetes_data(self, namespace):
+        config = kubernetes.config.load_kube_config('kubeconfig.conf')
+        api = kubernetes.client.CoreV1Api(kubernetes.client.ApiClient(config))
+        try:
+            pods = api.list_namespaced_pod(namespace)
+        except kubernetes.client.rest.ApiException as e:
+            self.send_error(500, "Could not get current pods: %s\n" % e)
+            return
+        return pods.items
+
     def do_POST(self):
+        self._setup_openstack_connection()
         data = self.rfile.read(int(self.headers['Content-Length']))
-        print(data)
         try:
             admissionRequest = json.loads(data)
         except json.decoder.JSONDecodeError:
@@ -15,16 +35,59 @@ class WebhookHandler(http.server.BaseHTTPRequestHandler):
 
         try:
             uid = admissionRequest['request']['uid']
+            namespace = admissionRequest['request']['namespace']
         except KeyError:
             self.send_error(400, "Invalid AdmissionReview object")
             return
+
+        kind = admissionRequest['request']['kind']['kind']
+        if kind == 'Deployment' or kind == "ReplicaSet":
+            spec = admissionRequest['request']['object']['spec']
+            requested_pods = spec['replicas']
+        elif kind == "Pod":
+            requested_pods = 1
+        else:
+            self.send_error(
+                400, "Only works for deployments, replicasets, or pods")
+            return
+
+        print("Incoming request: Kind: %s, Pod Count: %d\n" %
+            (kind, requested_pods))
+
+        current_pods = len(self._get_kubernetes_data(namespace))
+
+        allowed = True
+
+        project = self.openstack_conn.get_project(namespace)
+        if project:
+            print("found matching project: %s" % project['id'])
+        else:
+            try:
+                project = self.openstack_conn.get_domain(namespace)
+                print("found matching domain: %s" % project['id'])
+            except openstack.exceptions.ResourceNotFound:
+                print("denying request: no matching project or domain")
+                allowed = False
+
+        service = self.openstack_conn.identity.find_service('kubernetes')
+        if allowed:
+            limit = self.openstack_conn.identity.limits(
+                project_id=project['id'],
+                service_id=service['id'],
+                resource_name='pods'
+            )
+            resource_limit = next(limit).resource_limit
+            print("requested: %d, current: %d, limit: %d" % (
+                requested_pods, current_pods, resource_limit))
+            if requested_pods + current_pods > resource_limit:
+                allowed = False
 
         admissionResponse = {
             'apiVersion': 'admission.k8s.io/v1',
             'kind': 'AdmissionReview',
             'response': {
                 'uid': uid,
-                'allowed': True
+                'allowed': allowed
             }
         }
         httpResponse = json.dumps(admissionResponse)
@@ -40,6 +103,7 @@ def run(handler_class=http.server.BaseHTTPRequestHandler):
     httpd = http.server.HTTPServer(("", 8080), handler_class)
     httpd.socket = ssl.wrap_socket(
         httpd.socket, certfile=certFile, keyfile=keyFile, server_side=True)
+    print("Starting webhook server...")
     httpd.serve_forever()
 
 
